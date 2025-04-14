bbad2ef0244de03b540f0b4f1f846b01f1e6c729
amoncode6 <musyokaamon@gmail.com>
2025-04-14 05:02:34 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -1,104 +1,99 @@
-// get qr code of any bot using this ....................
-//coded by Mr Tech
-
-const express = require("express");
-const app = express();
-
+const PastebinAPI = require('pastebin-js'),
+pastebin = new PastebinAPI('EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL')
+const { makeid } = require('./id');
+const express = require('express');
+const fs = require('fs');
+let router = express.Router()
 const pino = require("pino");
-let { toBuffer } = require("qrcode");
-const path = require('path');
-const fs = require("fs-extra");
-const { Boom } = require("@hapi/boom");
+const {
+    default: Gifted_Tech,
+    useMultiFileAuthState,
+    delay,
+    makeCacheableSignalKeyStore,
+    Browsers
+} = require("maher-zubair-baileys");
 
-const PORT = process.env.PORT ||  5000
-const MESSAGE = process.env.MESSAGE ||  `
-┌───⭓『
-❒ *MR-TECH*
-❒ _NOW DEPLOY IT_
-└────────────⭓
-┌───⭓
-❒  • Chat with owner •
-❒ *GitHub:* __https://github.com/YourRepo__
-❒ *Author:* _wa.me/254759006509_
-❒ *YT:* _coming soon_
-└────────────⭓
-`
-
-if (fs.existsSync('./auth_info_baileys')) {
-    fs.emptyDirSync(__dirname + '/auth_info_baileys');
+function removeFile(FilePath) {
+    if (!fs.existsSync(FilePath)) return false;
+    fs.rmSync(FilePath, { recursive: true, force: true });
 };
 
-app.use("/", async(req, res) => {
+router.get('/', async (req, res) => {
+    const id = makeid();
+    let num = req.query.number;
 
-const { default: WasiWASocket, useMultiFileAuthState, Browsers, delay, DisconnectReason, makeInMemoryStore } = require("@whiskeysockets/baileys");
-const store = makeInMemoryStore({ logger: pino().child({ level: 'silent', stream: 'store' }) })
+    async function GIFTED_MD_PAIR_CODE() {
+        const { state, saveCreds } = await useMultiFileAuthState('./temp/' + id);
+        try {
+            let Pair_Code_By_Gifted_Tech = Gifted_Tech({
+                auth: {
+                    creds: state.creds,
+                    keys: makeCacheableSignalKeyStore(state.keys, pino({ level: "fatal" }).child({ level: "fatal" })),
+                },
+                printQRInTerminal: false,
+                logger: pino({ level: "fatal" }).child({ level: "fatal" }),
+                browser: ["Chrome (Linux)", "", ""]
+            });
 
-async function WASI() {
-    const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys')
-    try {
-        let Smd = WasiWASocket({
-            printQRInTerminal: false,
-            logger: pino({ level: "silent" }),
-            browser: [Browsers.Chrome, 'Windows 10', 'Chrome/89.0.4389.82'],
-            auth: state
-        });
-
-        Smd.ev.on("connection.update", async (s) => {
-            const { connection, lastDisconnect, qr } = s;
-            if (qr) { res.end(await toBuffer(qr)); }
-
-            if (connection == "open") {
-                await delay(3000);
-                let user = Smd.user.id;
-
-                // SESSION ID
-                let CREDS = fs.readFileSync(__dirname + '/auth_info_baileys/creds.json')
-                var Scan_Id = Buffer.from(CREDS).toString('base64')
-
-                console.log(`
-====================  SESSION ID  ==========================                   
-SESSION-ID ==> ${Scan_Id}
--------------------   SESSION CLOSED   -----------------------
-`)
-
-                let msgsss = await Smd.sendMessage(user, { text:  Scan_Id });
-                await Smd.sendMessage(user, { text: MESSAGE }, { quoted: msgsss });
-                await delay(1000);
-                try { await fs.emptyDirSync(__dirname + '/auth_info_baileys'); } catch (e) {}
-            }
-
-            Smd.ev.on('creds.update', saveCreds)
-
-            if (connection === "close") {
-                let reason = new Boom(lastDisconnect?.error)?.output.statusCode
-                if (reason === DisconnectReason.connectionClosed) {
-                    console.log("Connection closed!")
-                } else if (reason === DisconnectReason.connectionLost) {
-                    console.log("Connection Lost from Server!")
-                } else if (reason === DisconnectReason.restartRequired) {
-                    console.log("Restart Required, Restarting...")
-                    WASI().catch(err => console.log(err));
-                } else if (reason === DisconnectReason.timedOut) {
-                    console.log("Connection TimedOut!")
-                } else {
-                    console.log('Connection closed with bot. Please run again.');
-                    console.log(reason)
+            if (!Pair_Code_By_Gifted_Tech.authState.creds.registered) {
+                await delay(1500);
+                num = num.replace(/[^0-9]/g, '');
+                const code = await Pair_Code_By_Gifted_Tech.requestPairingCode(num);
+                if (!res.headersSent) {
+                    await res.send({ code });
                 }
             }
-        });
-    } catch (err) {
-        console.log(err);
-        await fs.emptyDirSync(__dirname + '/auth_info_baileys');
+
+            Pair_Code_By_Gifted_Tech.ev.on('creds.update', saveCreds);
+            Pair_Code_By_Gifted_Tech.ev.on("connection.update", async (s) => {
+                const { connection, lastDisconnect } = s;
+                if (connection == "open") {
+                    await delay(5000);
+                    let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
+                    await delay(800);
+                    let b64data = Buffer.from(data).toString('base64');
+                    let session = await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: '' + b64data });
+
+                    let GIFTED_MD_TEXT = `
+*_Pair Code Connected to MR-TECH*
+*_Made With 🤍_*
+______________________________________
+╔════◇
+║ *『 WOW YOU'VE CHOSEN MR TECH 』*
+║ _You Have Completed the First Step to Deploy a WhatsApp Bot._
+╚════════════════════════╝
+╔═════◇
+║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
+║❒ *Ytube:* _coming soon_
+║❒ *Owner:* _https://wa.me/254759006509_
+║❒ *Repo:* _https://github.com/amoncode6/Wa-Bot_
+║❒ *WaGroup:* _https://chat.whatsapp.com/DPiTEufUSo0FwTg8O5zZmW_
+║❒ *WaChannel:* _https://whatsapp.com/channel/0029VbAJHnaJf05VugeMMh1a_
+║❒ *Plugins:* _https://github.com/wasixd/WASI-MD-PLUGINS_
+╚════════════════════════╝
+_____________________________________
+
+_Don't Forget To Give Star To My Repo_`;
+
+                    await Pair_Code_By_Gifted_Tech.sendMessage(Pair_Code_By_Gifted_Tech.user.id, { text: GIFTED_MD_TEXT }, { quoted: session });
+
+                    await delay(100);
+                    await Pair_Code_By_Gifted_Tech.ws.close();
+                    return await removeFile('./temp/' + id);
+                } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
+                    await delay(10000);
+                    GIFTED_MD_PAIR_CODE();
+                }
+            });
+        } catch (err) {
+            console.log("service restated");
+            await removeFile('./temp/' + id);
+            if (!res.headersSent) {
+                await res.send({ code: "Service Unavailable" });
+            }
+        }
     }
-}
-
-WASI().catch(async(err) => {
-    console.log(err);
-    await fs.emptyDirSync(__dirname + '/auth_info_baileys');
-    //// MADE BY ITXWASI
+    return await GIFTED_MD_PAIR_CODE();
 });
 
-});
-
-app.listen(PORT, () => console.log(`App listened on port http://localhost:${PORT}`));
-                
+module.exports = router;
