60385882d54030c778737abc88a28b44aaeadf1d
amoncode6 <musyokaamon@gmail.com>
2025-04-14 06:51:59 +0300

    Update wasiqr.js

--- wasiqr.js
+++ wasiqr.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 let router = express.Router()
 const pino = require("pino");
 const {
-	default: Techboy_Tech,
+	default: Wasi_Tech,
 	useMultiFileAuthState,
 	jidNormalizedUser,
 	Browsers,
@@ -28,13 +28,13 @@ const {
 } = require("node:fs/promises")
 router.get('/', async (req, res) => {
 	const id = makeid();
-	async function MR_TECH_QR_CODE() {
+	async function WASI_MD_QR_CODE() {
 		const {
 			state,
 			saveCreds
 		} = await useMultiFileAuthState('./temp/' + id)
 		try {
-			let Qr_Code_By_Techboy_Tech = Techboy_Tech({
+			let Qr_Code_By_Wasi_Tech = Wasi_Tech({
 				auth: state,
 				printQRInTerminal: false,
 				logger: pino({
@@ -43,8 +43,8 @@ router.get('/', async (req, res) => {
 				browser: Browsers.macOS("Desktop"),
 			});
 
-			Qr_Code_By_Technoy_Tech.ev.on('creds.update', saveCreds)
-			Qr_Code_By_Techboy_Tech.ev.on("connection.update", async (s) => {
+			Qr_Code_By_Wasi_Tech.ev.on('creds.update', saveCreds)
+			Qr_Code_By_Wasi_Tech.ev.on("connection.update", async (s) => {
 				const {
 					connection,
 					lastDisconnect,
@@ -56,10 +56,10 @@ router.get('/', async (req, res) => {
 					let data = fs.readFileSync(__dirname + `/temp/${id}/creds.json`);
 					await delay(800);
 				   let b64data = Buffer.from(data).toString('base64');
-				   let session = await Qr_Code_By_Techboy_Tech.sendMessage(Qr_Code_By_Techboy_Tech.user.id, { text: '' + b64data });
+				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
 	
-				   let MR_TECH_TEXT = `
-*_Session Connected By Techboy Tech_*
+				   let WASI_MD_TEXT = `
+*_Session Connected By MR TECH_*
 *_Made With 🤍_*
 ______________________________________
 ╔════◇
@@ -78,16 +78,16 @@ ______________________________________
 _____________________________________
 	
 _Don't Forget To Give Star To My Repo_`
-	 await Qr_Code_By_Techboy_Tech.sendMessage(Qr_Code_By_Techboy_Tech.user.id,{text:MR_TECH_TEXT},{quoted:session})
+	 await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id,{text:WASI_MD_TEXT},{quoted:session})
 
 
 
 					await delay(100);
-					await Qr_Code_By_Techboy_Tech.ws.close();
+					await Qr_Code_By_Wasi_Tech.ws.close();
 					return await removeFile("temp/" + id);
 				} else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
 					await delay(10000);
-					MR_TECH_QR_CODE();
+					WASI_MD_QR_CODE();
 				}
 			});
 		} catch (err) {
@@ -100,6 +100,6 @@ _Don't Forget To Give Star To My Repo_`
 			await removeFile("temp/" + id);
 		}
 	}
-	return await MR_TECH_QR_CODE()
+	return await WASI_MD_QR_CODE()
 });
 module.exports = router
